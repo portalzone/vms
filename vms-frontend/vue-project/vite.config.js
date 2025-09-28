3f3d9332c6abb67848482f45efbd5924ccb308ad
M. C. Victor <63499060+portalzone@users.noreply.github.com>
2025-09-28 01:17:14 -0230

    Update vite.config.js

--- vms-frontend/vue-project/vite.config.js
+++ vms-frontend/vue-project/vite.config.js
@@ -1,23 +1,13 @@
-// vite.config.js
-
 import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue"; // <--- Import the plugin
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-    // other configuration...
     plugins: [
-        vue(), // <--- Add the plugin here
+        vue(),
     ],
-
     build: {
         rollupOptions: {
-            input: {
-                // Change this line to your new entry point path
-                plugins: [
-                    vue(), // <--- Add the plugin here
-                ],
-                main: "vms-frontend/vue-project/src/main.js",
-            },
+            input: "vms-frontend/vue-project/src/main.js",
         },
     },
 });
