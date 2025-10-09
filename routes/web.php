57ae7f7252122543ca0f75b9b7fcaff6da8c6aca
portalzone <muojekevictor@gmail.com>
2025-10-09 00:47:15 -0230

    test

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Response;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{any}', function () {
+    $indexPath = public_path('index.html');
+    if (File::exists($indexPath)) {
+        return Response::file($indexPath);
+    }
+    abort(404);
+})->where('any', '^(?!api).*$');
